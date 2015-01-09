a875035c26e4aa631cca5f0dcf1c95346f965bb9
blck <manu.mirandad@gmail.com>
2015-01-09 17:57:01 +0100

    Fixed issue with package build.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 packages = ['Django', 'django-widget-tweaks', ]
 
 setup(
-    name = "django_zoneadmin",
-    version = "0.1",
-    url = "http://github.com/argaen/django_zoneadmin",
-    description = "Alternative django administration interface with bootstrap",
-    author = "argaen",
-    author_email = "manu.mirandad@gmail.com",
-    packages = ['zone_admin'],
-    keywords = ['django', 'admin', 'bootstrap', 'angularjs']
+    name="django_zoneadmin",
+    version="0.1.1",
+    url="http://github.com/argaen/django_zoneadmin",
+    description="Alternative django administration interface with bootstrap",
+    author="argaen",
+    author_email="manu.mirandad@gmail.com",
+    packages=['zone_admin'],
+    keywords=['django', 'admin', 'bootstrap', 'angularjs'],
 )
