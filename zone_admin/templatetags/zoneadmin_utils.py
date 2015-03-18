616ff91cff7cbd74d49d97a9202cfc918bdc584d
argaen <manu.mirandad@gmail.com>
2015-03-18 12:56:32 +0100

    Started custom dashboard styling. Working menu for all pages (added templatetag for app_list).

--- zone_admin/templatetags/zoneadmin_utils.py
+++ zone_admin/templatetags/zoneadmin_utils.py
@@ -1,11 +1,17 @@
 from django import template
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, NoReverseMatch
 from django.forms import ModelChoiceField, DateField, ModelMultipleChoiceField, BooleanField
 from django.template.loader import get_template
 from django.template import Context
 from django.forms.fields import TimeField, SplitDateTimeField
 from django.utils.safestring import mark_safe
+from django.contrib import admin
+from django.utils import six
+from django.utils.text import capfirst
+from django.apps import apps
+from django.conf import settings
+
 
 register = template.Library()
 
@@ -181,9 +187,65 @@ def custom_widget(parser, token):
             elif value.startswith("'") and value.endswith("'"):
                 value = value[1:-1]
             extra_attributes[key] = value
-        is_inlines = contents[2].split('=')[1] == 'True'
-    else:
-        is_inlines = False
     field_name = contents[1]
     extra_attributes['class'] = 'form-control'
     return BootstrapWidgetNode(field_name, extra_attributes)
+
+
+print settings.TEMPLATE_CONTEXT_PROCESSORS
+
+
+@register.assignment_tag(takes_context=True)
+def get_app_list(context):
+    app_dict = {}
+    request = context['request']
+    for model, model_admin in admin.site._registry.items():
+        app_label = model._meta.app_label
+        has_module_perms = request.user.has_module_perms(app_label)
+
+        if has_module_perms:
+            perms = model_admin.get_model_perms(request)
+
+            # Check whether user has any perm for this module.
+            # If so, add the module to the model_list.
+            if True in perms.values():
+                info = (app_label, model._meta.model_name)
+                model_dict = {
+                    'name': capfirst(model._meta.verbose_name_plural),
+                    'object_name': model._meta.object_name,
+                    'perms': perms,
+                }
+                if perms.get('change', False):
+                    try:
+                        model_dict['admin_url'] = reverse('admin:%s_%s_changelist' % info, current_app=admin.site.name)
+                    except NoReverseMatch:
+                        pass
+                if perms.get('add', False):
+                    try:
+                        model_dict['add_url'] = reverse('admin:%s_%s_add' % info, current_app=admin.site.name)
+                    except NoReverseMatch:
+                        pass
+                if app_label in app_dict:
+                    app_dict[app_label]['models'].append(model_dict)
+                else:
+                    app_dict[app_label] = {
+                        'name': apps.get_app_config(app_label).verbose_name,
+                        'app_label': app_label,
+                        'app_url': reverse(
+                            'admin:app_list',
+                            kwargs={'app_label': app_label},
+                            current_app=admin.site.name,
+                        ),
+                        'has_module_perms': has_module_perms,
+                        'models': [model_dict],
+                    }
+
+    # Sort the apps alphabetically.
+    app_list = list(six.itervalues(app_dict))
+    app_list.sort(key=lambda x: x['name'].lower())
+
+    # Sort the models alphabetically within each app.
+    for app in app_list:
+        app['models'].sort(key=lambda x: x['name'])
+
+    return app_list
