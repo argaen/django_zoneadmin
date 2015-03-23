5ab069243b30451dfdce33baaa8558101e61cc41
argaen <manu.mirandad@gmail.com>
2015-03-23 16:20:52 +0100

    Merged branches to implement the theming functionality. Choose theme with ZONEADMIN_THEME variable in settings file.

---
+++ zone_admin/__init__.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+
+import os
+BASE_DIR = os.path.dirname(__file__)
+
+default_path = os.path.join(BASE_DIR, 'templates/default')
+if hasattr(settings, 'ZONEADMIN_THEME'):
+    path = os.path.join(BASE_DIR, 'templates/', settings.ZONEADMIN_THEME)
+    settings.TEMPLATE_DIRS = ((path,) if os.path.isdir(path) else (default_path,)) + settings.TEMPLATE_DIRS
+else:
+    settings.TEMPLATE_DIRS = (default_path,) + settings.TEMPLATE_DIRS
