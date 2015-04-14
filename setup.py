002346101cfbe7fe65b8b13b422339b2f5648467
argaen <manu.mirandad@gmail.com>
2015-04-14 11:45:21 +0200

    Fixed #27.
    Added quotes for the value attribute...

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ packages = ['Django']
 
 setup(
     name="django_zoneadmin",
-    version="0.2.3",
+    version="0.2.4",
     url="http://github.com/argaen/django_zoneadmin",
     description="Alternative django administration interface with bootstrap",
     author="argaen",
