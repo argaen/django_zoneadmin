478e30b9ae40f4bb267b643194b150abe79009de
argaen <manu.mirandad@gmail.com>
2015-04-14 11:23:33 +0200

    Improved layout for login html.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ packages = ['Django']
 
 setup(
     name="django_zoneadmin",
-    version="0.2.2",
+    version="0.2.3",
     url="http://github.com/argaen/django_zoneadmin",
     description="Alternative django administration interface with bootstrap",
     author="argaen",
     author_email="manu.mirandad@gmail.com",
     packages=['zone_admin'],
     include_package_data=True,
-    keywords=['django', 'admin', 'bootstrap', 'angularjs'],
+    keywords=['django', 'admin', 'bootstrap', 'djangozone'],
     install_requires=packages
 )
